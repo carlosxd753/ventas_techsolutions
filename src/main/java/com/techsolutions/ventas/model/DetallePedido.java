807cf19434c6eb7596a6f1ba506281f6b4ed29c2
Carlos Basurto <carlosxd753@gmail.com>
2025-11-24 23:14:49 -0500

    Se hizo un fix para la relacion entre Producto y DetallePedido

--- src/main/java/com/techsolutions/ventas/model/DetallePedido.java
+++ src/main/java/com/techsolutions/ventas/model/DetallePedido.java
@@ -3,7 +3,6 @@ package com.techsolutions.ventas.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,7 +22,7 @@ public class DetallePedido extends BaseModel{
     @JoinColumn(name = "id_pedido", nullable = false)
     private Pedido pedido;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "id_producto", nullable = false)
     private Producto producto;
 }
