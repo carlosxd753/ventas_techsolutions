87c729844db747e2799eb8c1b11ffccfa473c3c9
Carlos Basurto <carlosxd753@gmail.com>
2025-11-26 01:03:58 -0500

    se agrego validacion de rol al dto

--- src/main/java/com/techsolutions/ventas/dto/RolRequestDTO.java
+++ src/main/java/com/techsolutions/ventas/dto/RolRequestDTO.java
@@ -1,6 +1,11 @@
 package com.techsolutions.ventas.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RolRequestDTO(
+        @NotBlank(message = "el nombre obligatorio")
+        @Size(min = 2, max = 100, message = "el nombre debe tener entre 2 y 100 caracteres")
         String nombre
 ) {
 }
