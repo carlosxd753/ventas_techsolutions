5abd3fe5fa3f728d8b326fd87e23c38f3b2ebed5
Carlos Basurto <carlosxd753@gmail.com>
2025-11-27 23:32:57 -0500

    Se agrego datos para el seed de las tablas: rol, cliente, metodo_pago, usuario, pedido y detalle_pedido

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,3 +19,37 @@ INSERT INTO producto (nombre, precio_venta, stock, precio_compra, stock_minimo, 
 ('Smartwatch Xiaomi Watch S1', 520.00, 15, 380.00, 5, 'ACTIVO', 'https://i.imgur.com/QZlKaCv.png', NOW(), NOW()),
 ('Disco Duro Externo WD 2TB', 340.00, 20, 260.00, 7, 'ACTIVO', 'https://i.imgur.com/k61RjDB.png', NOW(), NOW()),
 ('Router TP-Link Archer AX5400', 680.00, 9, 520.00, 3, 'ACTIVO', 'https://i.imgur.com/QXlpWiw.png', NOW(), NOW());
+
+INSERT INTO rol (nombre, estado, fecha_creacion, fecha_actualizacion) VALUES
+('ADMINISTRADOR', 'ACTIVO', NOW(), NOW()),
+('GERENTE', 'ACTIVO', NOW(), NOW()),
+('CONTADOR', 'ACTIVO', NOW(), NOW()),
+('COMPRAS', 'ACTIVO', NOW(), NOW()),
+('VENTAS', 'ACTIVO', NOW(), NOW());
+
+INSERT INTO cliente (nombre, celular, dni, estado, fecha_creacion, fecha_actualizacion) VALUES
+('Pedro', '987123456', '12345670', 'ACTIVO', NOW(), NOW()),
+('Carlos', '987123455', '12345671', 'ACTIVO', NOW(), NOW()),
+('Jorge', '987123454', '12345672', 'ACTIVO', NOW(), NOW()),
+('Ricardo', '987123453', '12345673', 'ACTIVO', NOW(), NOW()),
+('Armando', '987123452', '12345674', 'ACTIVO', NOW(), NOW());
+
+INSERT INTO metodo_pago (nombre, estado, fecha_creacion, fecha_actualizacion) VALUES
+('Stripe', 'ACTIVO', NOW(), NOW()),
+('Paypal', 'ACTIVO', NOW(), NOW());
+
+INSERT INTO usuario (correo, password, estado, id_rol, fecha_creacion, fecha_actualizacion) VALUES
+('administrador@techsolutions.com', '123456', 'ACTIVO', 1, NOW(), NOW()),
+('gerente@techsolutions.com', '123456', 'ACTIVO', 2, NOW(), NOW()),
+('contador@techsolutions.com', '123456', 'ACTIVO', 3, NOW(), NOW()),
+('compras@techsolutions.com', '123456', 'ACTIVO', 4, NOW(), NOW()),
+('ventas@techsolutions.com', '123456', 'ACTIVO', 5, NOW(), NOW());
+
+INSERT INTO pedido (id_cliente, id_metodo_pago, fecha_creacion, fecha_actualizacion) VALUES
+(1, 1, NOW(), NOW()),
+(1, 2, NOW(), NOW());
+
+INSERT INTO detalle_pedido (id_pedido, id_producto, sub_total, cantidad, fecha_creacion, fecha_actualizacion) VALUES
+(1, 1, 2800.00, 1, NOW(), NOW()),
+(1, 2, 460.00, 2, NOW(), NOW()),
+(2, 3, 570.00, 3, NOW(), NOW());
