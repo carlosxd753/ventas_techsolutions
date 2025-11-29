8907c1d747abd5d7b4f0abf5bea212747a86f56e
Carlos Basurto <carlosxd753@gmail.com>
2025-11-29 16:54:22 -0500

    Se quito subtotal en el detalle pedido create dto

--- src/main/java/com/techsolutions/ventas/dto/DetallePedidoCreateRequestDTO.java
+++ src/main/java/com/techsolutions/ventas/dto/DetallePedidoCreateRequestDTO.java
@@ -1,10 +1,7 @@
 package com.techsolutions.ventas.dto;
 
-import java.math.BigDecimal;
-
 public record DetallePedidoCreateRequestDTO(
         Long idProducto,
-        int cantidad,
-        BigDecimal subTotal
+        int cantidad
 ) {
 }
